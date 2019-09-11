f1bdc0268432118fd6ef8342eadc023124fe9ba8
noble999 <nleduc2@mail.csuchico.edu>
2019-09-11 16:53:57 -0700

    uses a if statement to allow user to determine which of the two submitted integers is larger

--- biggestnumber.cpp
+++ biggestnumber.cpp
@@ -6,25 +6,29 @@ int main()
   //declare variables here
 	int number1;
 	int number2;
-
-  std::cout<<"Please enter a whole number:\n";
+	int biggest;
+  std::cout<<"Please enter a whole number: ";
   //get first input using std::cin>>
  	cin>> number1;
-  std::cout<<"Please enter another whole number:\n";
+	cout<<"\n";
+  std::cout<<"Please enter another whole number: ";
   //get second input using std::cin>> again with a second variable
 	cin>> number2;
+	cout<<"\n";
   //determine if first or second is bigger and store that in a third variable
 	if(number1>number2)
 	{
-		std::cout<<"Of those two numbers, the biggest is: "<<number1<<"\n";
-		std::cout<<std::endl<<"Thank you for playing.\n";
+		std::cout<<"Of those two numbers, the biggest is: "<<number1;
+		cout<<"\n";
+		std::cout<<"\nThank you for playing. \n";
+
 	}
 	else
 	{
- 		std::cout<<"Of those two numbers, the biggest is: "<<number2;
+ 		std::cout<<"Of those two numbers, the biggest is: "<<number2<<"\n";
  		 //print out the variable representing the biggest number
-
-  		std::cout<<std::endl<<"Thank you for playing.\n";
+		cout<< "\n";
+  		std::cout<<"Thank you for playing.\n";
 	}
   return 0;
 }
